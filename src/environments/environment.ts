7decf10872e9a281a869d22d1c880bb67d928ddb
jose valdez <jose281297@gmail.com>
2021-04-05 22:59:22 -0400

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+
 export const environment = {
   production: false,
-   firebaseConfig : {
+  firebaseConfig : {
     apiKey: "AIzaSyC-54aj0nE9gk0WoIP_MWC-jWsSF-h4_74",
     authDomain: "lenguaje5-f9c79.firebaseapp.com",
     projectId: "lenguaje5-f9c79",
@@ -12,8 +14,18 @@ export const environment = {
     messagingSenderId: "827580830292",
     appId: "1:827580830292:web:4dfe2e65be99017be3d3c1",
     measurementId: "G-QJYBY23E0Y"
+
+
   }
-};
+
+
+  
+  }
+
+  
+
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -22,4 +34,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+ import 'zone.js/dist/zone-error';  // Included with Angular CLI.
